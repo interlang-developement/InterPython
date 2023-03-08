e69f371bf8574c69057772c2628529bac68b2a3e
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2023-03-08 20:51:05 +0800

    feat: indentation handler

--- grammar/InterPython.g4
+++ grammar/InterPython.g4
@@ -2,13 +2,71 @@ grammar InterPython;
 
 @header {
 package org.interpython.antlr;
+
+import org.interpython.antlr.parsing.IndentationVisitor;
 }
 
-code : (statement ( ('\r' | '\n' | '\r\n' | ';') statement )* ('\r' | '\n' | '\r\n' | ';')*)? EOF;
+tokens { INDENT, DEDENT }
+
+@lexer::members {
+  	private boolean pendingDent = true;
+  	private int indentCount = 0;
+  	private java.util.LinkedList<Token> tokenQueue = new java.util.LinkedList<>();
+  	private java.util.Stack<Integer> indentStack = new java.util.Stack<>();
+  	private Token initialIndentToken = null;
+  	private int getSavedIndent() { return indentStack.isEmpty() ? 0 : indentStack.peek(); }
+
+  	private CommonToken createToken(int type, String text, Token next) {
+  		CommonToken token = new CommonToken(type, text);
+  		if (null != initialIndentToken) {
+  			token.setStartIndex(initialIndentToken.getStartIndex());
+  			token.setLine(initialIndentToken.getLine());
+  			token.setCharPositionInLine(initialIndentToken.getCharPositionInLine());
+  			token.setStopIndex(next.getStartIndex()-1);
+  		}
+  		return token;
+  	}
+
+  	@Override
+  	public Token nextToken() {
+  		if (!tokenQueue.isEmpty()) { return tokenQueue.poll(); }
+
+  		Token next = super.nextToken();
+  		if (pendingDent && null == initialIndentToken && NEWLINE != next.getType()) { initialIndentToken = next; }
+  		if (null == next || HIDDEN == next.getChannel() || NEWLINE == next.getType()) { return next; }
+
+  		if (next.getType() == EOF) {
+  			indentCount = 0;
+  			if (!pendingDent) {
+  				initialIndentToken = next;
+  				tokenQueue.offer(createToken(NEWLINE, "NEWLINE", next));
+  			}
+  		}
+
+  		while (indentCount != getSavedIndent()) {
+  			if (indentCount > getSavedIndent()) {
+  				indentStack.push(indentCount);
+  				tokenQueue.offer(createToken(InterPythonParser.INDENT, "INDENT" + indentCount, next));
+  			} else {
+  				indentStack.pop();
+  				tokenQueue.offer(createToken(InterPythonParser.DEDENT, "DEDENT" + getSavedIndent(), next));
+  			}
+  		}
+  		pendingDent = false;
+  		tokenQueue.offer(next);
+  		return tokenQueue.poll();
+  	}
+}
+
+code : lines EOF;
+
+lines : NEWLINE* (statement ( NEWLINE statement )* NEWLINE*)?;
 
 statement :
-      expression |
-      assignment;
+//      IF expression ':' NEWLINE INDENT lines DEDENT
+      | expression
+      | assignment
+      ;
 
 assignment :
     NAME '=' expression;
@@ -121,4 +179,14 @@ NUMBER : [0-9]+;
 FLOAT: NUMBER '.' NUMBER;
 STRING : '\'' (~('\r' | '\n' | '\'') ('\\\'')? )* '\'' | '"' (~('\r' | '\n' | '"') ('\\"')? )* '"';
 
-WS: [ \t\n\r]+ -> skip ;
+NEWLINE : ( '\r'? '\n' | '\r' ) {
+    if (pendingDent) { setChannel(HIDDEN); }
+    pendingDent = true;
+    indentCount = 0;
+    initialIndentToken = null;
+} ;
+
+WS : [ \t]+ {
+    setChannel(HIDDEN);
+    if (pendingDent) { indentCount += getText().length(); }
+} ;
--- src/main/java/org/interpython/antlr/parsing/syntax/SyntaxErrorParser.java
+++ src/main/java/org/interpython/antlr/parsing/syntax/SyntaxErrorParser.java
@@ -5,7 +5,6 @@ import org.antlr.v4.runtime.CommonToken;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
 
-import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -28,13 +27,33 @@ public class SyntaxErrorParser extends BaseErrorListener {
 
         if(offendingSymbol != null) {
             offendingSymbol = ((CommonToken) offendingSymbol).getText();
-            System.out.println(
-                    "  File \"" + processingFileName + "\", line " + line + "\n"
-                            + "    " + lineStr + "\n"
-                            + "    " + " ".repeat(charPositionInLine) + "^".repeat(((String) offendingSymbol).length()) + "\n"
-                            + "SyntaxError: Unexpected " + ((String) offendingSymbol).replace("<EOF>", "end of line")
-                            .replace(System.lineSeparator(), "end of line")
-            );
+            if (((String) offendingSymbol).startsWith("ui")) {
+                System.out.println(
+                        "  File \"" + processingFileName + "\", line " + line + "\n"
+                                + "    " + lineStr + "\n"
+                                + "    " + "^".repeat(Integer.parseInt(String.valueOf(((String) offendingSymbol).charAt(2)))) + "\n"
+                                + "SyntaxError: Unexpected indentation size " + ((String) offendingSymbol).charAt(2)
+
+                );
+            } else if (((String) offendingSymbol).startsWith("INDENT")) {
+                System.out.println(
+                        "  File \"" + processingFileName + "\", line " + line + "\n"
+                                + "    " + lineStr + "\n"
+                                + "    " + " ".repeat(charPositionInLine + 1) + "^^^^" + "\n"
+                                + "SyntaxError: Unexpected Indentation"
+                );
+            } else if( ((String) offendingSymbol).isBlank() && ((String) offendingSymbol).length() % 4 == 0){
+
+
+            } else {
+                System.out.println(
+                        "  File \"" + processingFileName + "\", line " + line + "\n"
+                                + "    " + lineStr + "\n"
+                                + "    " + " ".repeat(charPositionInLine) + "^".repeat(((String) offendingSymbol).replace("<EOF>", "-").length()) + "\n"
+                                + "SyntaxError: Unexpected " + ((String) offendingSymbol).replace("<EOF>", "end of line")
+                                .replace(System.lineSeparator(), "end")
+                );
+            }
         }else if((lineStr.charAt(charPositionInLine) == '\'') || (lineStr.charAt(charPositionInLine) == '\"'))
             System.out.println(
                     "  File \"" + processingFileName + "\", line " + line + "\n"
@@ -42,6 +61,8 @@ public class SyntaxErrorParser extends BaseErrorListener {
                             + "    " + " ".repeat(charPositionInLine) + "^" + "\n"
                             + "SyntaxError: Unclosed string definition"
             );
+
+//        System.err.println("line " + line + ":" + charPositionInLine + " " + msg);
 
         System.exit(1);
     }
